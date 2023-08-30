eee1f840389325c6f224590212cfb686bbe6f85e
lpetralli <lucas.petralli@webee.io>
2023-08-30 20:30:46 -0300

    x

--- pages/0_🔌_DB_Connection.py
+++ pages/0_🔌_DB_Connection.py
@@ -16,7 +16,8 @@ def db_connection_variables():
         'host': 'localhost',
         'port': '3306',
         'database_name': None,
-        'dialect': None
+        'dialect': None, 
+        'database_path': None
     }
     
     # Initialize session variables if they are not set
@@ -29,7 +30,7 @@ db_connection_variables()
 st.header('LLMate 🧉')
 dialects = ['sqlite', 'mysql', 'postgresql', 'oracle', 'mssql']
 
-my_grid = grid(2, 4, 1, vertical_align="bottom")
+my_grid = grid(2, 4, 1, 1, vertical_align="bottom")
 
 st.session_state.username = my_grid.text_input('username')
 st.session_state.password = my_grid.text_input('password', type= "password")
@@ -37,6 +38,7 @@ st.session_state.host = my_grid.text_input('host', "localhost")
 st.session_state.port = my_grid.text_input('port', "3306")  # default port for MySQL
 st.session_state.database_name = my_grid.text_input('db name')
 st.session_state.dialect = my_grid.selectbox('Select a database dialect:', dialects)
+st.session_state.database_path = st.text_input('Path to SQLite database file')
 
 col1, col2, col3 = st.columns(3)
 
@@ -46,13 +48,20 @@ with col1:
 with col2:
     if st.button('Connect to db', use_container_width=True):
         try:
-            st.session_state['db_uri'] = f"{st.session_state.dialect}://{st.session_state.username}:{st.session_state.password}@{st.session_state.host}:{st.session_state.port}/{st.session_state.database_name}"
+            if st.session_state.dialect == 'sqlite':
+                st.session_state['db_uri'] = f"sqlite:///{st.session_state.database_path}"
+                st.session_state['db_conn'] = st.session_state.database_path
+            else:
+                st.session_state['db_uri'] = f"{st.session_state.dialect}://{st.session_state.username}:{st.session_state.password}@{st.session_state.host}:{st.session_state.port}/{st.session_state.database_name}"
+                st.session_state['db_conn'] = st.session_state.database_name
+                
             st.session_state['sql_db'] = SQLDatabase.from_uri(st.session_state['db_uri'])
-            st.session_state['db_conn'] = st.session_state.database_name
             st.session_state['include_tables'] = st.session_state['sql_db'].get_table_names()
             st.session_state['table_names'] = st.session_state['sql_db'].get_table_names()
             st.success('Connected')
+
         except:
+
             st.error('Connection failed')
 
 with col3:
